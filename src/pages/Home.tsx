192c75f8302f8dcbe346bb8098f3bc7a4dc3ce21
Thiago Vinícius <80261904+ubyss@users.noreply.github.com>
2023-02-27 10:40:00 -0300

    Feat: added initial components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ContentBar from '../components/Home/ContentBar'
+import Main from '../components/Home/Main'
+import Topbar from '../components/Home/Topbar'
+
+const Home = () => {
+  return (
+    <>
+        <Topbar />
+        <ContentBar />
+        <Main />
+    </>
+  )
+}
+
+export default Home
